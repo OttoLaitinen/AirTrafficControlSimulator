f4e32264845e90a43f075fe3ba9845450e22dcf7
Otto Laitinen <otto.laitinen@aalto.fi>
2018-03-07 22:22:21 +0200

    Added many classes. All are unfinished.

---
+++ src/Airplane.scala
@@ -0,0 +1,32 @@
+
+
+class Airplane(
+  airport: Airport,
+  airline: String,
+  fuelCapacity: Int,
+  fuelConsumption: Double,
+  var altitude: Int,
+  timeToDestination: Int,
+  totalCargoWeight: Int,
+  minRunwaylength: Int,
+  passengers: Int,
+  var currentFlight: Option[Flight],
+  var nextFlight: Option[Flight]) {
+  
+  /*Additional values and variables*/
+  var fuel = fuelCapacity //mahdollisesti minus joku luku mut meh...
+  
+  /*Functions*/
+  def changeAltitude(newAltitude: Int): Unit = altitude = newAltitude
+  
+  def ascend(runwayNo: Int): Unit = airport.ascendPlane(airport.getRunwayNo(runwayNo), this)
+  
+  def descend(runwayNo: Int): Unit = airport.descendPlane(airport.getRunwayNo(runwayNo), this)
+  
+  def sendToQueue(number: Int): Unit = airport.sendToQueue(airport.getQueueNo(number), this)
+  
+  def sendToGate(number: Int): Unit = airport.sendToGate(airport.getGateNo(number), this)
+  
+}
+
+
---
+++ src/Airport.scala
@@ -0,0 +1,43 @@
+import scala.collection.mutable._
+
+class Airport(
+  runways: Vector[Runway],
+  crossingRunways: Map[Runway, Vector[Runway]],
+  gates: Vector[Gate],
+  queuesOnGround: Vector[LandQueue],
+  queuesInAir: Vector[InAirQueue],
+  rushFactor: Double) {
+  
+  /*Values and variables*/
+  var time: Int = 0
+  val planes: Buffer[Airplane] = Buffer[Airplane]()
+
+  /*Functions*/
+  private def checkPlanes: Unit = ???
+
+  private def checkRunways: Unit = ???
+
+  def ascendPlane(runway: Runway, plane: Airplane): Unit = ???
+
+  def descendPlane(runway: Runway, plane: Airplane): Unit = ???
+
+  private def createPlane: Unit = ???
+
+  private def onTick(): Unit = ???
+
+  def ascendPlanesInQueues(runway: Runway): Unit = ???
+
+  def sendToGate(gate: Gate, plane: Airplane): Unit = ???
+  
+  def sendToQueue(queue: Queue, plane: Airplane): Unit = ???
+  
+  def getRunwayNo(runwayNo: Int): Runway = ???
+  
+  def getQueueNo(number: Int): Queue = ???
+  
+  def getGateNo(number: Int): Gate = ???
+
+  /*Miten teen tuon onTick metodin timerin?? Pitää varmaankin myös kehittää testiluokka/vast
+   *Jolla ohjelmaa pystyy ajamaan ilman käyttöliittymää */
+
+}
